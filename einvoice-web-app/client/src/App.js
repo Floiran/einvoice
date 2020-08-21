1bc0054856bc5fc3398537d3b543b73dec6d63be
Filip Matusak <filip.matusak@gmail.com>
2020-08-21 09:44:30 +0200

    javascript format

--- einvoice-web-app/client/src/App.js
+++ einvoice-web-app/client/src/App.js
@@ -1,53 +1,52 @@
-import React, {Component} from 'react';
-import './App.css';
-import Form from "./Form";
-import InvoiceList from "./InvoiceList";
+import React, {Component} from 'react'
+import './App.css'
+import Form from './Form'
+import InvoiceList from './InvoiceList'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            apiUrl: null,
-            invoices: []
-        }
-
-        this.addInvoice = this.addInvoice.bind(this);
+    this.state = {
+      apiUrl: null,
+      invoices: []
     }
 
-    addInvoice(invoice) {
-        let invoices = this.state.invoices;
-        invoices.push(invoice);
-        this.setState({invoices});
-    }
+    this.addInvoice = this.addInvoice.bind(this);
+  }
 
-    componentDidMount() {
-        fetch('/api/url')
-            .then( response => response.text())
-            .then( url => {
-                fetch(url + '/api/invoices')
-                    .then( response => response.json())
-                    .then( data => this.setState({invoices: data}));
+  addInvoice(invoice) {
+    let invoices = this.state.invoices;
+    invoices.push(invoice);
+    this.setState({invoices});
+  }
 
-                this.setState({apiUrl: url})
-            });
-    }
+  componentDidMount() {
+    fetch('/api/url')
+      .then( response => response.text())
+      .then( url => {
+        fetch(url + '/api/invoices')
+          .then( response => response.json())
+          .then( data => this.setState({invoices: data}));
 
-    render() {
-        let { apiUrl } = this.state;
-        if(apiUrl) {
-            return (
-                <div className="App container">
-                    <header className="App-header">
-                        <h1>E-invoice</h1>
-                    </header>
-                    <Form apiUrl={apiUrl} addInvoice={this.addInvoice} />
-                    <InvoiceList invoices={this.state.invoices} apiUrl={apiUrl}/>
-                </div>
-            );
-        } else return <div></div>;
-    }
+        this.setState({apiUrl: url})
+      });
+  }
 
+  render() {
+    let { apiUrl } = this.state;
+    if(apiUrl) {
+      return (
+        <div className="App container">
+          <header className="App-header">
+            <h1>E-invoice</h1>
+          </header>
+          <Form apiUrl={apiUrl} addInvoice={this.addInvoice} />
+          <InvoiceList invoices={this.state.invoices} apiUrl={apiUrl}/>
+        </div>
+      );
+    } else return <div></div>;
+  }
 }
 
 export default App;
--- einvoice-web-app/client/src/InvoiceList.js
+++ einvoice-web-app/client/src/InvoiceList.js
@@ -1,43 +1,43 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class InvoiceList extends Component {
-    render() {
-        let { invoices, apiUrl } = this.props;
+  render() {
+    let { invoices, apiUrl } = this.props;
 
-        let rows = [];
-        if(invoices) {
-            rows = invoices.map((invoice, i) => {
-                return <tr key={i}>
-                    <th scope="row">{i+1}</th>
-                    <td>{invoice.id}</td>
-                    <td>{invoice.sender}</td>
-                    <td>{invoice.receiver}</td>
-                    <td><a href={apiUrl + "/api/invoice/full/" + invoice.id}>download</a></td>
-                </tr>
-            });
-        }
-
-        return (
-            <div className="container">
-                <h2>All invoices</h2>
-                <table className="table table-striped table-borderless table-hover">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>ID</th>
-                        <th>Sender</th>
-                        <th>Receiver</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {rows}
-                    </tbody>
-                </table>
-            </div>
-        )
+    let rows = [];
+    if(invoices) {
+      rows = invoices.map((invoice, i) => {
+        return <tr key={i}>
+          <th scope="row">{i+1}</th>
+          <td>{invoice.id}</td>
+          <td>{invoice.sender}</td>
+          <td>{invoice.receiver}</td>
+          <td><a href={apiUrl + "/api/invoice/full/" + invoice.id}>download</a></td>
+        </tr>
+      });
     }
+
+    return (
+      <div className="container">
+        <h2>All invoices</h2>
+        <table className="table table-striped table-borderless table-hover">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>ID</th>
+            <th>Sender</th>
+            <th>Receiver</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          {rows}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default InvoiceList;
